60f9a8f2146b3ae4dc08263241a0d3a66c58cb2f
Luis Lucas Silva <luislucas_andrade@hotmail.com>
2021-10-14 11:22:12 -0300

    Added a Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -cp target/classes:target/dependency/* -jar ./app/target/psico-no-precinho-backend-app-1.0-SNAPSHOT.jar
--- app/src/main/kotlin/com/luislucassilva/psiconoprecinho/adapters/router/handlers/AdministratorHandler.kt
+++ app/src/main/kotlin/com/luislucassilva/psiconoprecinho/adapters/router/handlers/AdministratorHandler.kt
@@ -1,9 +1,11 @@
 package com.luislucassilva.psiconoprecinho.adapters.router.handlers
 
+import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.buildAndAwait
 
+@Component
 class AdministratorHandler {
 
     suspend fun findByUserNameAndPasswordRequest(serverRequest: ServerRequest): ServerResponse {
